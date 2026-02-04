a2aa382d992e9ed66e7118538900f99d28ce5b1f
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2026-02-04 22:53:14 +0100

    Update routing.js

--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -4,9 +4,6 @@ import { createNavigation } from "next-intl/navigation";
 export const routing = defineRouting({
   locales: ["en", "fr"],
   defaultLocale: "fr",
-  // 'as-needed' : pas de /fr/ pour la langue par défaut
-  // 'always' (défaut) : toujours un /fr/ ou /en/
-  localePrefix: "as-needed",
 });
 
 // Lightweight wrappers around Next.js' navigation APIs
